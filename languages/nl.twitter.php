4c9ebb92da79c8468326330f569f5caec282bf31
Jerome Bakker <jeabakker@coldtrick.com>
2013-03-11 14:17:54 +0100

    updated: twitter language file

--- languages/nl.twitter.php
+++ languages/nl.twitter.php
@@ -1,6 +1,9 @@
 <?php
+
 if(elgg_is_active_plugin("twitter")){
 	$language = array (
+	  'twitter:invalid' => 'Deze widget is geconfigureerd met een ongeldige Twitter gebruikersnaam. Klik op de bewerk icoon om dit aan te passen.',
+	  'twitter:apibug' => '*Wegens een bug in de Twitter 1.0 API, is het mogelijk dat je minder tweets ziet als ingesteld.',
 	  'twitter:title' => 'Twitter',
 	  'twitter:info' => 'Toon je laatste tweets',
 	  'twitter:notset' => 'Deze Twitter Widget is nog niet ingesteld. Om je laatste Tweets te laten zien klik op "Bewerk" en vul je gegevens in',
