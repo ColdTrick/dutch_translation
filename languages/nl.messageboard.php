1c95482a5247605a87c1e53a04a5b2a87490bc57
Wouter Bruijning <wouterbruijning1974@gmail.com>
2014-04-09 06:20:37 +0200

    Update nl.messageboard.php
    Een paar taalaanpassingen

--- languages/nl.messageboard.php
+++ languages/nl.messageboard.php
@@ -2,20 +2,20 @@
 
 if (elgg_is_active_plugin("messageboard")) {
 	$language = array (
-	  'messageboard:owner' => '%s\'s berichten box',
-	  'messageboard:owner_history' => '%s\'s berichten op %s\'s berichten box',
-	  'river:messageboard:user:default' => '%s plaatste op %s\'s berichten box',
+	  'messageboard:owner' => 'Berichtenbox van %s',
+	  'messageboard:owner_history' => 'Berichten van %s in de berichtenbox van %s',
+	  'river:messageboard:user:default' => '%s plaatste in berichtenbox van %s',
 	  'messageboard:history:title' => 'Geschiedenis',
 	  'messageboard:replyon' => 'antwoord op',
-	  'messageboard:board' => 'Berichten box',
-	  'messageboard:messageboard' => 'berichten box',
+	  'messageboard:board' => 'Berichtenbox',
+	  'messageboard:messageboard' => 'berichtenbox',
 	  'messageboard:viewall' => 'Alles bekijken',
 	  'messageboard:postit' => 'Plaats',
 	  'messageboard:history' => 'geschiedenis',
 	  'messageboard:none' => 'Er zijn nog geen berichten geplaatst',
 	  'messageboard:num_display' => 'Aantal berichten om weer te geven',
-	  'messageboard:desc' => 'Dit is een berichten box die je op je profiel kunt plaatsen waar andere gebruikers op kunnen reageren.',
-	  'messageboard:user' => '%s\'s berichten box',
+	  'messageboard:desc' => 'Dit is een berichtenbox die je op je profiel kunt plaatsen waar andere gebruikers op kunnen reageren.',
+	  'messageboard:user' => 'Berichtenbox van %s',
 	  'messageboard:posted' => 'Je bericht is succesvol geplaatst.',
 	  'messageboard:deleted' => 'Je bericht is succesvol verwijderd.',
 	  'messageboard:email:subject' => 'Er is een nieuwe reactie op een bericht!',
@@ -23,11 +23,11 @@ if (elgg_is_active_plugin("messageboard")) {
 
 %s
 
-Om de reacties te bekijken, klik hier:
+Om de reacties te bekijken klik je hier:
 
 %s
 
-Om %s\'s profiel te bekijken, klik hier:
+Om het profiel van %s te bekijken klik je hier:
 
 %s
 
@@ -35,7 +35,7 @@ Je kunt niet antwoorden op deze email.',
 	  'messageboard:blank' => 'Sorry, je moet wel tekst opgeven voordat we je bericht kunnen opslaan.',
 	  'messageboard:notfound' => 'Sorry, we konden het opgegeven item niet vinden.',
 	  'messageboard:notdeleted' => 'Sorry, we konden het bericht niet verwijderen.',
-	  'messageboard:somethingwentwrong' => 'Er is een fout opgetreden tijdens het opslaan van je bericht, controlleer op je wel een bericht hebt geschreven.',
+	  'messageboard:somethingwentwrong' => 'Er is een fout opgetreden tijdens het opslaan van je bericht. Controleer of je wel een bericht hebt geschreven.',
 	  'messageboard:failure' => 'Er is een fout opgetreden tijdens het toevoegen van je bericht. Probeer het nogmaals.',
 	);
 	add_translation("nl", $language);
