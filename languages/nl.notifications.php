5a448fb91d7ac1c00079d10feef6214842029853
Wouter Bruijning <wouterbruijning1974@gmail.com>
2014-04-09 07:16:57 +0200

    Update nl.notifications.php
    Een paar taalaanpassingen

--- languages/nl.notifications.php
+++ languages/nl.notifications.php
@@ -3,18 +3,18 @@
 if (elgg_is_active_plugin("notifications")) {
 	$language = array (
 	  'notifications:subscriptions:friends:title' => 'Vrienden',
-	  'notifications:subscriptions:friends:description' => 'Het volgende is een automatische verzameling van je vrienden. Om updates te ontvangen selecteer hieronder. Dit zal invloed hebben op de betreffende gebruikers in het hoofd notificatie-instellingen overzicht aan de onderkant van deze pagina.',
+	  'notifications:subscriptions:friends:description' => 'Hier vind je een automatische verzameling van je vrienden. Wil je updates krijgen? Dat kun je hieronder aangeven. Dit zal invloed hebben op de betreffende gebruikers in het hoofdoverzicht van notificatie-instellingen, aan de onderkant van deze pagina.',
 	  'friends:all' => 'Alle vrienden',
-	  'notifications:subscriptions:personal:description' => 'Ontvang een notificatie als er acties worden ondernomen op jou content',
+	  'notifications:subscriptions:personal:description' => 'Ontvang een notificatie als er acties worden ondernomen op jouw content, bijvoorbeeld als er een reactie gegveen wordt.',
 	  'notifications:subscriptions:personal:title' => 'Persoonlijke notificatie',
-	  'notifications:subscriptions:collections:edit' => 'Om je vrienden collecties aan te passen, klik hier.',
+	  'notifications:subscriptions:collections:edit' => 'Wil je jouw vriendenverzamelingen aanpassen? Klik dan hier.',
 	  'notifications:subscriptions:changesettings' => 'Notificaties',
 	  'notifications:subscriptions:changesettings:groups' => 'Groepsnotificaties',
 	  'notification:method:email' => 'E-mail',
 	  'notifications:subscriptions:title' => 'Notificatie per gebruiker',
-	  'notifications:subscriptions:description' => 'Om notificaties te ontvangen wanneer je vrienden nieuwe content plaatsen, vindt ze hieronder en selecteer de meldings manier die je wilt gebruiken.',
-	  'notifications:subscriptions:groups:description' => 'Om een notificatie te ontvangen wanneer er nieuwe content is in een groep waarvan je lid bent, zoek de groep hieronder op en selecteer de meldingsmanier die je wilt gebruiken.',
-	  'notifications:subscriptions:success' => 'Je notificatie instellingen zijn opgeslagen.',
+	  'notifications:subscriptions:description' => 'Wil je een notificatie ontvangen wanneer je vrienden nieuwe content plaatsen? Selecteer dan de wijze die je daarvoor wilt gebruiken.',
+	  'notifications:subscriptions:groups:description' => 'Wil je een notificatie ontvangen wanneer er nieuwe content is in een groep waarvan je lid bent? Zoek de groep hieronder op en selecteer de wijze die je wilt gebruiken.',
+	  'notifications:subscriptions:success' => 'Je notificatie-instellingen zijn opgeslagen.',
 	);
 	add_translation("nl", $language);
 }
