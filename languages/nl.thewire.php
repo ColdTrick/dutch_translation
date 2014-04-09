185846a40be402be7094376a6dfc7a51acb37d4f
Wouter Bruijning <wouterbruijning1974@gmail.com>
2014-04-09 07:21:06 +0200

    Update nl.thewire.php

--- languages/nl.thewire.php
+++ languages/nl.thewire.php
@@ -4,7 +4,7 @@ if (elgg_is_active_plugin("thewire")) {
 	$language = array (
 	  'thewire:by' => 'Wire-bericht door %s',
 	  'thewire:replying' => 'Antwoord op %s (@%s) die schreef',
-	  'thewire:thread' => 'Conversatie',
+	  'thewire:thread' => 'Gesprek',
 	  'thewire:tags' => 'Wire-berichten met de tag \'%s\'',
 	  'thewire:noposts' => 'Nog geen Wire-berichten',
 	  'thewire:update' => 'Bijwerken',
