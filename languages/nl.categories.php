6d99ac90a2cfb812594dc2a83ef00bfb29e58201
Wouter Bruijning <wouterbruijning1974@gmail.com>
2014-04-09 05:58:29 +0200

    Update nl.categories.php
    Een paar taalaanpassingen

--- languages/nl.categories.php
+++ languages/nl.categories.php
@@ -3,11 +3,11 @@
 if (elgg_is_active_plugin("categories")) {
 	$language = array (
 	  'categories' => 'Categorieën',
-	  'categories:settings' => 'Stel Site categorieën in',
-	  'categories:explanation' => 'Om een aantal voorgedefinieerde categorieën in te stellen, die door over hele site kunnen worden gebruik, geeft ze hieronder in. Door een komma gescheiden. Geschikte plugins zullen ze dan weer geven als een gebruiker content bewerkt of aanmaakt.',
-	  'categories:save:success' => 'Site categorieën zijn succesvol opgeslagen.',
-	  'categories:on_activate_reminder' => 'Site categorieën zal niet werken totdat je enkele categorieën toevoegt. <a href="%s">Voeg categorieën toe</a>.',
-	  'categories:results' => 'Resultaten voor de Site categorie: %s',
+	  'categories:settings' => 'Stel sitecategorieën in',
+	  'categories:explanation' => 'Geef hieronder een aantal voorgedefinieerde categorieën in. Deze kunnen over hele site worden gebruikt. Je kunt meerdere categorieën opgeven, door een komma gescheiden. Geschikte plugins zullen deze dan weergeven als een gebruiker content bewerkt of aanmaakt.',
+	  'categories:save:success' => 'Sitecategorieën zijn succesvol opgeslagen.',
+	  'categories:on_activate_reminder' => 'Sitecategorieën zal niet werken totdat je enkele categorieën toevoegt. <a href="%s">Voeg categorieën toe</a>.',
+	  'categories:results' => 'Resultaten voor de sitecategorie: %s',
 	);
 	add_translation("nl", $language);
 }
